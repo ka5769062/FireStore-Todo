709ec79cc1571608437f9369bbb1276b654dbf7e
Kamran Ali <75374110+ka5769062@users.noreply.github.com>
2023-07-25 17:54:43 +0500

    code updated

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-  import { getFirestore,collection,doc, addDoc,getDocs} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getFirestore, collection, doc, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyC9xWDg2qFQdOz0XaWtFfw90cnuLVxih-o",
-    authDomain: "firestore-todo-app-da00e.firebaseapp.com",
-    projectId: "firestore-todo-app-da00e",
-    storageBucket: "firestore-todo-app-da00e.appspot.com",
-    messagingSenderId: "695545540689",
-    appId: "1:695545540689:web:91e5130a98e5281760c870",
-    measurementId: "G-1J7DCHZGQ6"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyC9xWDg2qFQdOz0XaWtFfw90cnuLVxih-o",
+  authDomain: "firestore-todo-app-da00e.firebaseapp.com",
+  projectId: "firestore-todo-app-da00e",
+  storageBucket: "firestore-todo-app-da00e.appspot.com",
+  messagingSenderId: "695545540689",
+  appId: "1:695545540689:web:91e5130a98e5281760c870",
+  measurementId: "G-1J7DCHZGQ6"
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-
- const db = getFirestore();
-
-
-
+const db = getFirestore();
 
 let userArr = [];
 
@@ -38,25 +34,25 @@ butn.addEventListener("click", function () {
 
   }
 
-  else {
+  // else {
 
-    let show = document.getElementById('show')
-    let li = document.createElement('li')
-    show.appendChild(li)
-    li.textContent = enterData
+  //   let show = document.getElementById('show')
+  //   let li = document.createElement('li')
+  //   show.appendChild(li)
+  //   li.textContent = enterData
 
-    let a = document.createElement('a');
-    a.innerHTML = "\u00d7";
-    a.href = "javascript:void(0)"
-    a.className = "remove"
-    li.appendChild(a)
+  //   let a = document.createElement('a');
+  //   a.innerHTML = "\u00d7";
+  //   a.href = "javascript:void(0)"
+  //   a.className = "remove"
+  //   li.appendChild(a)
 
-    //    localStorage      
+  //   //    localStorage      
 
-    // userArr.push({ 'name': enterData })
-    localStorage.setItem("user", JSON.stringify(userArr))
+  //   // userArr.push({ 'name': enterData })
+  //   localStorage.setItem("user", JSON.stringify(userArr))
 
-  }
+  // }
 
   document.getElementById('todo').value = ""
 
@@ -71,31 +67,50 @@ butn.addEventListener("click", function () {
   savMsg(enterData)
 })
 
-async function savMsg (enterData) {
+async function savMsg(enterData) {
 
-    try {
-        const docRef = await addDoc(collection(db, "users" ), {
-        first: enterData,
-        
-        });
-        // console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        // console.error("Error adding document: ", e);
-    }
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: enterData,
+
+    });
+    // console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    // console.error("Error adding document: ", e);
+  }
+
+  getData()
+
+}
+
+
+
+let display = document.querySelector(".getingData")
+
+
+async function getData() {
   
-
-    getData(enterData)  
-}
-
-
-async function getData(enterData){
-
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
- let myData = `${doc.id} => ${doc.data(enterData)}`
- 
-    console.log(myData);
-});
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    
+    let frst = `<div>${doc.data().first}<div>`
+   
+    display.innerHTML += frst
+    //  renderData()   
+    
+    console.log(display)
+    
+  })
 
 
-}
+};
+
+
+
+
+// setTimeout(() => {
+  
+// }, 3000);
+
+
+
